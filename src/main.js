40201491048e99c05e8c4073e25d9557bd278ed6
GoncaloBMN <53783696+GoncaloBMN@users.noreply.github.com>
2019-08-20 01:01:39 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -15,20 +15,31 @@ const i_privateKey = i_key.getPrivate('hex');
 var vehicle_1 = {
     brand: "Opel",
     model: "Corsa C Diesel",
-    version: "Corsa 1.3 CDTi Cosmo",
+    version: "1.3 CDTi Cosmo",
     cilinder: 1248,
     hp: 70,
     n_doors: 5,
     n_seats: 5,
     license_plate: "10-10-XE",
     vin: "XXXXXXXX",
-    type: "Hatchback",
-    driver: "Gonçalo de Brito Miranda Nabais"
+    driver: "Gonçalo Nabais"
+};
+var vehicle_2 = {
+    brand: "test",
+    model: "Corsa C Diesel",
+    version: "1.3 CDTi Cosmo",
+    cilinder: 1200,
+    hp: 20,
+    n_doors: 5,
+    n_seats: 3,
+    license_plate: "10-10-XA",
+    vin: "vin",
+    driver: "Gonçalo Nabais"
 };
 
 var ccidadao_1 = {
-    id: "14743525",
-    nif: "260381519",
+    id: "100",
+    nif: "200",
     dta_emissao: "01-01-2000",
     dta_validade: "01-01-2020"
 };
@@ -41,9 +52,9 @@ var cconducao_1 = {
 };
 
 var driver_1 = {
-    name: "Gonçalo de Brito Miranda Nabais",
-    address: "Rua Ferreira de Castro, n60, 3ESQ",
-    bdate: "29-12-1996",
+    name: "Gonçalo Nabais",
+    address: "Rua das Túlipas",
+    bdate: "01-01-2000",
     c_cidadao: ccidadao_1,
     c_conducao: cconducao_1
 };
@@ -63,52 +74,26 @@ var beneficiary = {
 }
 
 let Insuchain = new Blockchain();
-
 let contract = new Contract(insurer, beneficiary);
 
 contract.addVehicle(vehicle_1);
 contract.addDriver(driver_1);
+contract.setContractPolicyID();
 contract.signContract(b_key);
 Insuchain.addContract(contract);
-
 Insuchain.minePendingContracts();
 
+// CREATE ACTA
+let contract_2 = new Contract(insurer, beneficiary);
 
+contract_info = Insuchain.getContract(1, beneficiary.name, beneficiary.nif);
+Insuchain.passContractInfo(contract_info, contract_2);
 
+contract_2.addVehicle(vehicle_2);
+contract_2.signContract(b_key);
+Insuchain.createACTA(contract_2, contract_2.policyID);
+Insuchain.minePendingContracts();
 
-
-//console.log(contract.getVehicleIndexByLP("a"));
-//console.log(contract.getVehicleByIndex(contract.getVehicleIndexByLP("a")));
-//console.log(contract.vehicleHasDriver(vehicle_1));
-//console.log(JSON.stringify(contract.vehicles, null, 4));
-//console.log(contract.removeVehicle(contract.getVehicleIndexByLP("10-10-XE")));
-//console.log(contract.getDriverIndex("Gonçalo de Brito Miranda Nabais", "260381519"));
-//Insuchain.getLatestContractFromPending().addDriver(driver_1);
-//Insuchain.createACTA();
-
-//console.log(Insuchain.chain[1]);
-//console.log(Insuchain.isChainValid());
-//console.log(Insuchain.chain[1].hasValidContracts());
-
-//console.log(Insuchain.getAllContractsFrom(beneficiary.name, beneficiary.nif));
-//console.log(JSON.stringify(Insuchain.chain[1], null, 4));
-/***************************************/
-//data1 = "data1";
-//data2 = "data2";
-//Insuchain.addBlock(data1);
-//Insuchain.addBlock(data2);
-
-//console.log(JSON.stringify(Insuchain, null, 4));
-//console.log(JSON.stringify(Insuchain.chain[1], null, 4));
-
-//console.log("Is Insuchain valid? " + Insuchain.isChainValid());
-
-//Insuchain.chain[2].data = [4, 5, "tes"];
-//Insuchain.chain[1].hash = Insuchain.chain[1].calculateHash();
-//console.log("Is Insuchain valid? " + Insuchain.isChainValid());
-
-//console.log(JSON.stringify(Insuchain, null, 4));
-//var array = new Array(3);
-
-//console.log(array.length);
-/***************************************/
+// GET ALL CONTRACTS FROM
+let contract_from = Insuchain.getAllContractsFrom(b_pubKey);
+console.log(contract_from);
